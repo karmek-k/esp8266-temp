c687a2a057516d0e58fe4b3376330a2bfa34fb23
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-25 21:27:01 +0200

    Use DhtSensor in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 
+#include "dht_sensor.h"
+
 constexpr int baudRate{9600};
+
+constexpr int dhtPowerPin{D7};
+constexpr int dhtOutPin{D8};
+
+karmek::DhtSensor sensor{dhtOutPin, DHT11};
 
 void setup()
 {
     Serial.begin(baudRate);
-    Serial.println("Hello, world!");
+
+    pinMode(dhtPowerPin, OUTPUT);
+    digitalWrite(dhtPowerPin, HIGH);
 }
 
 void loop()
 {
-    Serial.println("123");
+    Serial.println(sensor.temperature());
     delay(1000UL);
 }
