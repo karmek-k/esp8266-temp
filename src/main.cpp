239a088bd7c668aaedd0a450de97b5961e89a756
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-04-26 17:13:18 +0200

    Display temp & humidity to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
+#include <sstream>
+#include <iomanip>
+
 #include <Arduino.h>
 
 #include "dht_sensor.h"
+#include "screen.h"
 
 constexpr int baudRate{9600};
 
 constexpr int dhtPowerPin{D7};
 constexpr int dhtOutPin{D8};
 
+constexpr int screenClockPin{D1};
+constexpr int screenDataPin{D2};
+
+std::stringstream ss;
 karmek::DhtSensor sensor{dhtOutPin, DHT11};
+karmek::Screen screen{screenClockPin, screenDataPin};
 
 void setup()
 {
@@ -21,5 +30,9 @@ void loop()
 {
     delay(sensor.minDelayMs());
 
-    Serial.println(sensor.temperature());
+    std::stringstream().swap(ss);
+    ss << std::setprecision(2) << sensor.humidity() << "% ";
+    ss << std::setprecision(2) << sensor.temperature() << "C";
+
+    screen.print(0, 64 - 24, ss.str().c_str());
 }
